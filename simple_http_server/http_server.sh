2a37f5e0055da3912073ae2e31edfb1c06d73a8a
Andre Light <andrelight.al@gmail.com>
2022-10-17 01:48:29 +0000

    Update container and server to use **ncat**
    - On Debian, `ncat` is recommended for `-k`/`--keep-open` option:
      - "Accept multiple connections in listen mode"

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -16,4 +16,4 @@ function server () {
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+ncat -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
